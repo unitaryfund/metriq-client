cb3aef20f8563d16441b374f22a11b8a23ffd91d
Daniel Strano <stranoj@gmail.com>
2021-08-16 11:13:46 -0400

    Add submissionContentUrl to example

--- examples/submission_add.py
+++ examples/submission_add.py
@@ -3,6 +3,7 @@ from metriq.models.submission import SubmissionCreateRequest
 
 submission = SubmissionCreateRequest()
 submission.submissionName = "Test Submission (Client)"
+submission.submissionContentUrl = "https://arxiv.org"
 submission.description = "This is the first submission to be successfully uploaded with the Python client."
 client = MetriqClient(token="[Get this token from your web app account, and replace this string with it.]")
 print(client.submission_add(submission))
