f28bfc41729ad80e6e1108ca8a779ac19134cca6
Maria Maryam <maria.maryam0303@gmail.com>
2022-08-08 13:32:18 -0400

    Delete method_delete.py

--- examples/method_delete.py
+++
@@ -1,6 +0,0 @@
-from metriq import MetriqClient
-
-client = MetriqClient(token="[Get this token from your web app account, and replace this string with it.]")
-# Give a method ID, as the string below.
-print(client.method_delete("6101c6ac1134861ba8fbdd9e"))
-
