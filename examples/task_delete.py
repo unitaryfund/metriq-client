a8205e7178cb424defa5a603a2a0dabde6cd0dbd
Maria Maryam <maria.maryam0303@gmail.com>
2022-08-08 13:28:20 -0400

    Delete task_delete.py

--- examples/task_delete.py
+++
@@ -1,6 +0,0 @@
-from metriq import MetriqClient
-
-client = MetriqClient(token="[Get this token from your web app account, and replace this string with it.]")
-# Give a task ID, as the string below.
-print(client.task_delete("6102f848ff8f28281ae4ea4f"))
-
