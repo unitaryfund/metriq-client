b4c1b2cca4976fe83e5bb6db3b57ce7d54ea99b1
Maria Maryam <maria.maryam0303@gmail.com>
2022-08-08 13:16:18 -0400

    Update submission_get.py

--- examples/submission_get.py
+++ examples/submission_get.py
@@ -1,6 +1,4 @@
 from metriq import MetriqClient
 
-client = MetriqClient(
-    token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzcsInJvbGUiOiJjbGllbnQiLCJpYXQiOjE2NTk3MTA0Mzl9.__seCACSgBAr-6sZ1A1kaS4Y6qyUrbspO0vnFcG-tR8")
-
+client = MetriqClient(token="[Get this token from your web app account, and replace this string with it.]")
 print(client.submission_get("1"))
