d47432558117f06582b45fb38dd9026807e2b431
srilakshmip03 <96498480+srilakshmip03@users.noreply.github.com>
2023-08-02 10:21:10 -0700

    Update method_get_submission_count.py

--- examples/method_get_submission_count.py
+++ examples/method_get_submission_count.py
@@ -1,5 +1,5 @@
 from metriq import MetriqClient
 
 client = MetriqClient(token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTgsInJvbGUiOiJjbGllbnQiLCJpYXQiOjE2ODY1NzYzODl9._4CZoykTkKv_OPSWU4jNBgFhJjFH5aVOp0LubuFVA6Y")
-result = client.method_submission_count_get()
+result = client.method_get_submission_count()
 assert result is not None
