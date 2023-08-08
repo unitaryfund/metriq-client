024d83d65896b491329326df6568b728d443d29f
Ali <130245910+QuantumAli@users.noreply.github.com>
2023-08-07 18:44:17 -0700

    Update MetriqClient

--- examples/platform_add.py
+++ examples/platform_add.py
@@ -7,6 +7,6 @@ environment.name = "Test Submission (Client)"
 environment.fullName = "Test Submission Full Name (Client)"
 environment.description = "This is the first submission to be successfully uploaded with the Python client."
 
-client = MetriqClient(token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTgsInJvbGUiOiJjbGllbnQiLCJpYXQiOjE2ODY1NzYzODl9._4CZoykTkKv_OPSWU4jNBgFhJjFH5aVOp0LubuFVA6Y")
+client = MetriqClient(token=os.environ["METRIQ_CLIENT_API_KEY"])
 result = client.platform_add(environment)
 assert result is not None
