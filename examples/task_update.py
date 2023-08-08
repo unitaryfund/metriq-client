66fc7ccc8cd77c161241bbbe25df21782c2f5e65
Ali <130245910+QuantumAli@users.noreply.github.com>
2023-08-07 18:59:11 -0700

    Update MetriqClient

--- examples/task_update.py
+++ examples/task_update.py
@@ -7,6 +7,6 @@ task_id = "6102f848ff8f28281ae4ea3c"
 task.name = "MAXCUT 2"
 task.fullName = "Test Task Updated(Client)"
 task.description = "Test Task Updated Description (Client)."
-client = MetriqClient(token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTksInJvbGUiOiJjbGllbnQiLCJpYXQiOjE2ODY2NjQxODd9.VWT8b4peYwebm9-Ul4-6xkMMajATIiqUXO_dE4lxigk")
+client = MetriqClient(token=os.environ["METRIQ_CLIENT_API_KEY"])
 result = client.task_update(task_id, task)
 assert result is not None
