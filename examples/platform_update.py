bf130b8cb97fad09a43b4ff44e77b7576e91bd04
Ali <130245910+QuantumAli@users.noreply.github.com>
2023-08-07 18:45:12 -0700

    Update MetriqClient

--- examples/platform_update.py
+++ examples/platform_update.py
@@ -8,6 +8,6 @@ environment.parentPlatform = "6102f848ff8f28281ae4ea3c"
 environment.name = "Environment_1"
 environment.fullName = "Test environment Updated(Client)"
 environment.description = "Test environment Updated Description (Client)."
-client = MetriqClient(token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTgsInJvbGUiOiJjbGllbnQiLCJpYXQiOjE2ODY1NzYzODl9._4CZoykTkKv_OPSWU4jNBgFhJjFH5aVOp0LubuFVA6Y")
+client = MetriqClient(token=os.environ["METRIQ_CLIENT_API_KEY"])
 result = client.platform_update(platform_id, environment)
 assert result is not None
