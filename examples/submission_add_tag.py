13fc49b1528275bdf1c529245c16af978f770aa3
Ali <130245910+QuantumAli@users.noreply.github.com>
2023-08-09 19:17:05 -0700

    Update submission_add_tag.py

--- examples/submission_add_tag.py
+++ examples/submission_add_tag.py
@@ -1,7 +1,8 @@
 from metriq import MetriqClient
+import os
 
 submission_id = "1234567890"
 tag_name = "quantum-computing"
-client = MetriqClient(token=os.environ["METRIQ_CLIENT_API_KEY"])
+client = MetriqClient(token=str(os.environ["METRIQ_CLIENT_API_KEY"]))
 result = client.submission_add_tag(submission_id=submission_id, tag_name=tag_name)
 assert result is not None
