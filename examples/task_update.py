e3e5142b66d7d966cd20ca8388381365e2766bd6
Ali <130245910+QuantumAli@users.noreply.github.com>
2023-08-09 19:22:49 -0700

    Update task_update.py

--- examples/task_update.py
+++ examples/task_update.py
@@ -1,5 +1,6 @@
 from metriq import MetriqClient
 from metriq.models.task import TaskUpdateRequest
+import os
 
 
 task = TaskUpdateRequest()
@@ -7,6 +8,6 @@ task_id = "6102f848ff8f28281ae4ea3c"
 task.name = "MAXCUT 2"
 task.fullName = "Test Task Updated(Client)"
 task.description = "Test Task Updated Description (Client)."
-client = MetriqClient(token=os.environ["METRIQ_CLIENT_API_KEY"])
+client = MetriqClient(token=str(os.environ["METRIQ_CLIENT_API_KEY"]))
 result = client.task_update(task_id, task)
 assert result is not None
