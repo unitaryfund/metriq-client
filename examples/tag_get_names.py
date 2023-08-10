1bf094fc4d269ef65a55c44b2ff7dd075a029df7
Ali <130245910+QuantumAli@users.noreply.github.com>
2023-08-09 19:20:50 -0700

    Update tag_get_names.py

--- examples/tag_get_names.py
+++ examples/tag_get_names.py
@@ -1,6 +1,7 @@
 from metriq import MetriqClient
+import os
 
-client = MetriqClient(token=os.environ["METRIQ_CLIENT_API_KEY"])
+client = MetriqClient(token=str(os.environ["METRIQ_CLIENT_API_KEY"]))
 result = client.tag_names_get()
 assert result is not None
 
