5ef83063141f4eb6a77d51b711008db65e937b86
Ali <130245910+QuantumAli@users.noreply.github.com>
2023-08-09 19:17:58 -0700

    Update submission_get.py

--- examples/submission_get.py
+++ examples/submission_get.py
@@ -1,5 +1,6 @@
 from metriq import MetriqClient
+import os
 
-client = MetriqClient(token=os.environ["METRIQ_CLIENT_API_KEY"])
+client = MetriqClient(token=str(os.environ["METRIQ_CLIENT_API_KEY"]))
 result = client.submission_get("60f5ce7bc3519e62a97feb54")
 assert result is not None
