720afa8961db729afd37bef51e4eb5148d3c6f0e
Ali <130245910+QuantumAli@users.noreply.github.com>
2023-08-09 19:11:56 -0700

    Put in correct str()

--- examples/method_add.py
+++ examples/method_add.py
@@ -1,10 +1,11 @@
 from metriq import MetriqClient
 from metriq.models.method import MethodCreateRequest
+import os
 
 method = MethodCreateRequest()
 method.name = "Test Method Name (Client)"
 method.fullName = "Test Method Full Name (Client)"
 method.description = "This is the first method to be successfully uploaded with the Python client."
-client = MetriqClient(token=os.environ["METRIQ_CLIENT_API_KEY"])
+client = MetriqClient(token=str(os.environ["METRIQ_CLIENT_API_KEY"]))
 result = client.method_add(method)
 assert result is not None
