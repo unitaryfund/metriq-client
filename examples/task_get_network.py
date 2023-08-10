a809fff664f1b681e7b1146248ff14df0b54f5d2
Ali <130245910+QuantumAli@users.noreply.github.com>
2023-08-09 19:22:15 -0700

    Update task_get_network.py

--- examples/task_get_network.py
+++ examples/task_get_network.py
@@ -1,5 +1,6 @@
 from metriq import MetriqClient
+import os
 
-client = MetriqClient(token=os.environ(["METRIQ_CLIENT_API_KEY"])
+client = MetriqClient(token=str(os.environ["METRIQ_CLIENT_API_KEY"]))
 result = client.task_get_network()
 assert result is not None
