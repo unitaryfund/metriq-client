b8b8b966dc1ce3b5346822a204c88dba552513a6
WrathfulSpatula <stranoj@gmail.com>
2025-03-07 11:04:00 -0500

    Don't need tea-console or tea-client!

--- metriq_client/commands/app.py
+++ metriq_client/commands/app.py
@@ -1,9 +1,4 @@
 import typer
 
-from tea_console.commands.config import app as config_app
-
 
 app = typer.Typer(name="metriq", help="Metriq client.")
-
-# Add tea-console apps
-app.add_typer(config_app)
